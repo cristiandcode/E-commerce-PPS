ab11d39548b89d0cc1b60dafb98ff554d8186bf8
Cristian <diazcristian0210@gmail.com>
2025-11-10 20:01:29 -0300

    Creamos la estructura principal del proyecto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Evita que eslint bloquee el build
+  },
+  typescript: {
+    ignoreBuildErrors: false, // Si querés más rigidez, cambialo a true
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        // Opcional, mejora tiempos de recarga con Tailwind
+        '*.css': ['postcss-loader'],
+      },
+    },
+  },
 };
 
 export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="bg-red-200">
+      <h1 className="text-3xl font-bold">HomePage</h1>
+    </main>
+  );
+}
