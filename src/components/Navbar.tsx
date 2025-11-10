323fe9b9dbf172eaa6aaca128492cadabe7e39ef
Cristian <diazcristian0210@gmail.com>
2025-11-10 20:21:54 -0300

    Proceso de creacion del navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,24 @@
-const Navbar = () =>{
-    return(
-        <div className="">Navbar</div>
-    )
-}
+import Image from "next/image";
+import Link from "next/link";
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <nav className="w-full flex items-center justify-between">
+      {/* left */}
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/logo.png"
+          alt="Fashion Week"
+          width={36}
+          height={36}
+          className="w-6 h-6 md:w-9 md:h-9"
+        />
+        <p className="text-md font-medium tracking-wider">FASHION WEEK</p>
+      </Link>
+      {/* right */}
+      <div className="">Right</div>
+    </nav>
+  );
+};
+
+export default Navbar;
