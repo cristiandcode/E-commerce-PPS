8b3e12397627a9ff2a2b3ef6f677ff7cb451fb8b
Cristian <diazcristian0210@gmail.com>
2025-11-11 17:07:41 -0300

    Agregue el boton y css del añadir carrito

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,6 +3,7 @@
 import  Link  from "next/link";
 import  Image  from "next/image";
 import { ProductType } from "../types";
+import { ShoppingCart } from "lucide-react";
 
 const ProductCard = ({product}: {product:ProductType}) =>{
     return(
@@ -39,6 +40,13 @@ const ProductCard = ({product}: {product:ProductType}) =>{
                             ))}
                         </div>
                     </div>
+                </div>
+                {/* PRECIO Y AÑADIR AL CARRITO */}
+                <div className="flex items-center justify-between">
+                    <p className="font-medium">${product.price.toFixed(2)}</p>
+                    <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+                        <ShoppingCart className="w-4 h-4"/>
+                        Añadir al carrito</button>
                 </div>
             </div>
         </div>
