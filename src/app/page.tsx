b5d884d4f34f7b156fedc978f058eb77b3fa39a7
Cristian <diazcristian0210@gmail.com>
2025-11-10 21:36:28 -0300

    Agregue el producto destacado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function HomePage() {
-  return (
-    <main className="bg-red-200">
-      <h1 className="text-3xl font-bold">HomePage</h1>
-    </main>
-  );
+import Image from "next/image";
+const HomePage = () =>{
+  return(
+    <div className="">
+      <div className="relative aspect-3/1 mb-12">
+      <Image src="/featured.png" alt="Producto destacado" fill/>
+      </div>
+    </div>
+  )
 }
+
+export default HomePage;
